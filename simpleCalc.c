f7f9737f2ccb954d7ac682905e3c4159c7999eed
WonderedLamb256 <41864659+WonderedLamb256@users.noreply.github.com>
2020-02-09 15:39:10 -0500

    Add sqrt() function

--- simpleCalc.c
+++ simpleCalc.c
@@ -1,4 +1,4 @@
-/* Calculator that adds, subtracts, multiplies, divides, and squares
+/* Calculator that adds, subtracts, multiplies, divides, and squares. Square root function in testing.
 Made by WonderedLamb256 */
 
 #include <stdio.h>
@@ -18,7 +18,7 @@ int main()
 			int num2;
 			printf( "Choose number 1: " );
 			scanf( "%d", &num1 );
-			printf( "Choose number 2 (not needed in square operation): " );
+			printf( "Choose number 2 (not needed in square/sqrt operation): " );
 			scanf( "%d", &num2 );
 			int answer;
 			int operation;
@@ -34,6 +34,12 @@ int main()
 			{answer = num1 / num2;}
                         else if ( operation == 5 )
 			{answer = num1 * num1;}
+			else if ( operation == 6 ) {
+                                for ( int i=0;i>num1;i++ ) { // does not work with floating point numbers, will hang if given
+                                      if ( i * i == num1 )
+				      {answer = i;}
+				}
+			}
 			else
 			{
 				printf( "Error: invalid operation\n");
@@ -44,7 +50,7 @@ int main()
 		else if ( option == 2 )
 		{break;}
 		else if ( option == 3 )
-		{printf( "1 for add, 2 for subtract, 3 for multiply, 4 for divide, 5 for square\n");}
+		{printf( "1 for add, 2 for subtract, 3 for multiply, 4 for divide, 5 for square, 6 for square root\n");}
 		else
 		{
 			printf( "Error: invalid option\n" );
